808305a4c73d053e8096d8de1e76b6b6bc9bb5a3
Joshua Gagnier <gagnierjoshua@gmail.com>
2024-01-13 17:02:23 -0800

    GET & POST verified on postman working

--- main.go
+++ main.go
@@ -21,12 +21,25 @@ var todos = []todo{
 }
 
 func getTodos(c *gin.Context) { //gets the tasks from server and displays them *gin is a web framework
-	c.IndentedJSON(http.StatusOK, todos) //returns the tasks in json format
+	c.IndentedJSON(http.StatusOK, todos) //returns the tasks in json format with indentation
+}
+
+func addTodo(context *gin.Context) { //adds a new task to the server
+	var newTodo todo //creates a new task
+
+	if err := context.BindJSON(&newTodo); err != nil { //binds the new task to the server
+		return //returns the new task
+	}
+
+	todos = append(todos, newTodo) //adds the new task to the server
+
+	context.IndentedJSON(http.StatusCreated, newTodo) //returns the new task in json format with indentation
 }
 
 func main() {
 	router := gin.Default()        //creates a router/server
 	router.GET("/todos", getTodos) //gets the data from the server
+	router.POST("/todos", addTodo) //adds the data to the server
 	router.Run("localhost:9090")   //runs the server on port 9090
 }
 
